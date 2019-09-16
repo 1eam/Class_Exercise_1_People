728218b01b6986016e4085ff53870294dc40a18d
Esther <esther191299@gmail.com>
2019-09-16 14:09:50 +0200

    Build of Ethnicity class + Describe method

---
+++ src/people/Ethnicity.java
@@ -0,0 +1,16 @@
+package people;
+
+public class Ethnicity {
+	
+	String skincolor;
+	String colorEyes;
+	String typeEyes;
+	String typeVoice;
+	
+	
+	American have these attributes
+	European
+	Asian
+	African
+}
+//AMERICAN, EUROPEAN, ASAIN, AFRICAN with fixed attributes: skincolor, type of eyes, color of eyes, type voice
--- src/people/Person.java
+++ src/people/Person.java
@@ -6,4 +6,10 @@ public class Person {
 	float height;
 	String hobby;
 	String character;
+	
+	//this calss' function is to describe itself
+	void describeSelf(){
+		System.out.println("Hi there, let VERY BASICALLY describe myself.");
+		System.out.println("My name is" + this.name);
+	}
 }
