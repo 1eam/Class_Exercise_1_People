33ea19a8c4b18087f33ce84a81aef3767b5dfbcf
Esther <esther191299@gmail.com>
2019-09-16 18:52:42 +0200

    Casted Person class instances into constructor + cleaned Main Method

--- src/people/Main.java
+++ src/people/Main.java
@@ -4,11 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		Person john = new Person("John", 1.78, "Football", "wise");
-		john.name = "John";
-//		john.Ethnicity = new Ethnicity(American);
-		john.height = 1.78;
-		john.hobby = "Football";
-		john.characteristics = "Wise";
 
 	}
 
