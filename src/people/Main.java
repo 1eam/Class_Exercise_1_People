686f3b6dcc98550e4eff2634c247d7d0445e6dd5
Esther <esther191299@gmail.com>
2019-09-16 13:44:48 +0200

    First commit

---
+++ src/people/Main.java
@@ -0,0 +1,10 @@
+package people;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/people/Person.java
@@ -0,0 +1,9 @@
+package people;
+
+public class Person {
+	String name;
+	Ethnicity ethnicity; //TODO: make new class, containing strings declaring AMERICAN, EUROPEAN, ASAIN, AFRICAN with fixed attributes: skincolor, type of eyes, color of eyes, type voice
+	float height;
+	String hobby;
+	String character;
+}
