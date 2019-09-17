993b14e595106613ffe0643194d694ff8be18600
Esther <esther191299@gmail.com>
2019-09-17 17:48:41 +0200

    Project Rename

--- src/people/Ethnicity.java
+++ src/people/Ethnicity.java
@@ -1,17 +1,13 @@
 package people;
 
-public class Ethnicity { returns String;
+public class Ethnicity {
 
 //Input is string
-	public String skinColor;
-	public String colorEyes;
-	public String typeEyes;
-	public String typeVoice;
 	
 	public Ethnicity(String skinColor, String colorEyes, String typeEyes, String typeVoice){
 	}
 	
-	Ethnicity american = new Ethnicity("White", "Blue", "Round", "sound like people from reality TV")
+	Ethnicity american = new Ethnicity("White", "Blue", "Round", "Sound like people from reality TV");
 	Ethnicity european = new Ethnicity("White", "Blue, Grey or Brown", "Round", "like trying to speak whilst gagging");
 	Ethnicity asian = new Ethnicity("Yellow", "brown or darkbrown", "Round", "high and weird words emphasis");
 	Ethnicity african = new Ethnicity("Black", "Darkbrown", "Round", "2+2=4");
@@ -24,7 +20,7 @@ public class Ethnicity { returns String;
 		this.african = african;
 	}
 	
-	//functie Ethnicity () is this.eyecolor printen
-	System.out.println (american.skinColor);
+	
+	
 
 }
--- src/people/Main.java
+++ src/people/Main.java
@@ -3,8 +3,7 @@ package people;
 public class Main {
 
 	public static void main(String[] args) {
-		Person john = new Person("John", 1.78, "Football", "wise");
-
+		Person john = new Person("John", american, 1.78, "Football", "wise");
 	}
 
 }
